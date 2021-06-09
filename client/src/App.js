b15b4e7eca5e262c9ba6ff6e5ce8392da55e4f28
Martin Spassov <mspassov@uwo.ca>
2021-06-09 18:06:47 -0400

    set up redux files

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,38 @@
+import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
+import gallery from "./images/gallery.png";
+import Posts from "./components/Posts/Posts";
+import Form from "./components/Form/Form";
+import useStyles from "./styles";
+
 const App = () => {
+  const classes = useStyles();
+
   return (
-    <div>
-      <h1>Hello world</h1>
-    </div>
+    <Container maxWidth="lg">
+      <AppBar className={classes.appBar} position="static" color="inherit">
+        <Typography className={classes.heading} variant="h2" align="center">
+          Moments
+        </Typography>
+        <img className={classes.image} src={gallery} alt="logo" height="60" />
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid
+            container
+            justify="space-between"
+            alignItems="stretch"
+            spacing={3}
+          >
+            <Grid item xs={12} sm={7}>
+              <Posts />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
   );
 };
 
---
+++ client/src/components/Posts/Post/Post.js
@@ -0,0 +1,13 @@
+import React from "react";
+import useStyles from "./styles";
+
+function Post() {
+  const classes = useStyles();
+  return (
+    <div>
+      <h3>This is a single post</h3>
+    </div>
+  );
+}
+
+export default Post;
