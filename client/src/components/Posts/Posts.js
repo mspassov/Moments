67896a68be3e9df2ec3cd94d579261fc8cf8e527
Martin Spassov <mspassov@uwo.ca>
2021-06-10 21:34:08 -0400

    styled front-end posts

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,16 +1,30 @@
 import React from "react";
 import Post from "./Post/Post";
 import useStyles from "./styles";
+import { useSelector } from "react-redux";
+import { Grid, CircularProgress } from "@material-ui/core";
 
 function Posts() {
   const classes = useStyles();
-  return (
-    <div>
-      <h2>Post</h2>
-      <Post />
-      <Post />
-      <Post />
-    </div>
+  const posts = useSelector((state) => state.posts);
+
+  return !posts.length ? (
+    <CircularProgress />
+  ) : (
+    <Grid
+      className={classes.container}
+      container
+      alignItems="stretch"
+      spacing={3}
+    >
+      {posts.map((post) => {
+        return (
+          <Grid key={post.id} item xs={12} sm={6}>
+            <Post post={post} />
+          </Grid>
+        );
+      })}
+    </Grid>
   );
 }
 
