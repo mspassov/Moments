8591a1fa090d3cb4183230c7f77f4e8c318c8866
Martin Spassov <mspassov@uwo.ca>
2021-06-11 17:21:28 -0400

    created like functionality

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,7 +1,7 @@
 //Any routes related to creating/updating/deleting posts
 
 import express from 'express';
-import {getPosts, createPost, updatePost, deletePost} from '../controllers/posts.js';
+import {getPosts, createPost, updatePost, deletePost, likePost} from '../controllers/posts.js';
 
 const router = express.Router();
 
@@ -9,8 +9,7 @@ router.get('/', getPosts);
 router.post('/', createPost);
 router.patch('/:id', updatePost);
 router.delete('/:id', deletePost);
-
-
+router.patch('/:id/likePost', likePost);
 
 export default router;
 
