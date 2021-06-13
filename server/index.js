8f2b84ff1397d54b3e6ef2fd9f4c0709fc47e67d
Martin Spassov <mspassov@uwo.ca>
2021-06-13 12:39:49 -0400

    make it better

--- server/index.js
+++ server/index.js
@@ -12,6 +12,10 @@ app.use(express.urlencoded({limit: "30mb", extended: "true"}));
 app.use(cors());
 app.use('/posts', postRoutes);
 
+app.get('/', (req, res)=>{
+    res.send('This is the working API');
+})
+
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(process.env.CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
